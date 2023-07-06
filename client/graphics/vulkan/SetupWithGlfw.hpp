4e332c2a7f3a420645f532048a3da35ab3e1b5f8
kiterai <kiterai@foolslab.net>
2023-07-06 19:17:35 +0900

    format

--- client/graphics/vulkan/SetupWithGlfw.hpp
+++ client/graphics/vulkan/SetupWithGlfw.hpp
@@ -1,14 +1,13 @@
-
 #ifdef USE_DESKTOP_MODE
 
-#include <vulkan/vulkan.hpp>
+#include "Helper.hpp"
 #include <GLFW/glfw3.h>
 #include <vector>
-#include "Helper.hpp"
+#include <vulkan/vulkan.hpp>
 
 vk::UniqueInstance createVulkanInstanceWithGlfw();
 vk::UniqueSurfaceKHR createVulkanSurfaceWithGlfw(const vk::Instance instance, GLFWwindow *window);
-vk::UniqueDevice createVulkanDeviceWithGlfw(vk::PhysicalDevice physicalDevice, const UsingQueueSet& queueSet);
+vk::UniqueDevice createVulkanDeviceWithGlfw(vk::PhysicalDevice physicalDevice, const UsingQueueSet &queueSet);
 vk::PhysicalDevice chooseSuitablePhysicalDeviceWithGlfw(vk::Instance instance, vk::SurfaceKHR surface);
 SwapchainDetails createVulkanSwapchainWithGlfw(vk::PhysicalDevice physicalDevice, vk::Device device, vk::SurfaceKHR surface);
 std::vector<RenderTargetHint> getRenderTargetHintsWithGlfw(vk::PhysicalDevice physicalDevice, vk::Device device, const SwapchainDetails &swapchain);
