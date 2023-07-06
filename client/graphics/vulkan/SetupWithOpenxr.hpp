33772df4f56eef8ed37db5c833c190952e26d343
kiterai <kiterai@foolslab.net>
2023-07-06 18:27:43 +0900

    OpenXRによるVulkan初期化関数の公開

--- client/graphics/vulkan/SetupWithOpenxr.hpp
+++ client/graphics/vulkan/SetupWithOpenxr.hpp
@@ -1,3 +1,9 @@
 #include <vulkan/vulkan.hpp>
 #include <openxr/openxr_platform.h>
 #include <openxr/openxr.hpp>
+
+vk::Instance createVulkanInstanceWithOpenxr(xr::Instance xrInstance, xr::SystemId xrSystemId);
+vk::PhysicalDevice getPhysicalDeviceWithOpenxr(xr::Instance xrInstance, xr::SystemId xrSystemId, vk::Instance vkInstance);
+vk::Device createDeviceWithOpenxr(xr::Instance xrInstance, xr::SystemId xrSystemId, vk::PhysicalDevice physicalDevice);
+std::optional<int64_t> chooseXrSwapchainFormat(const std::vector<int64_t> formats);
+std::vector<vk::Image> getImagesFromXrSwapchain(xr::Swapchain swapchain);
