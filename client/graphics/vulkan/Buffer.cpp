e1af933443e38dfc112998264db38b95e995956f
kiterai <kiterai@foolslab.net>
2023-07-10 09:47:51 +0900

    書き込みメソッドを用意

--- client/graphics/vulkan/Buffer.cpp
+++ client/graphics/vulkan/Buffer.cpp
@@ -50,11 +50,19 @@ Buffer::Buffer(vk::PhysicalDevice physDevice, vk::Device device, vk::DeviceSize 
 
 ReadonlyBuffer::ReadonlyBuffer(vk::PhysicalDevice physDevice, vk::Device device, vk::Queue queue, vk::CommandBuffer cmdBuf, void *datSrc, vk::DeviceSize sz, vk::BufferUsageFlags usage, vk::Fence fence)
     : Buffer{physDevice, device, sz, usage | vk::BufferUsageFlagBits::eTransferDst, vk::MemoryPropertyFlagBits::eDeviceLocal} {
+    write(physDevice, device, queue, cmdBuf, datSrc, sz, 0, fence);
+}
+
+ReadonlyBuffer::ReadonlyBuffer(vk::PhysicalDevice physDevice, vk::Device device, vk::BufferUsageFlags usage, vk::DeviceSize sz)
+    : Buffer{physDevice, device, sz, usage | vk::BufferUsageFlagBits::eTransferDst, vk::MemoryPropertyFlagBits::eDeviceLocal} {
+}
+
+void ReadonlyBuffer::write(vk::PhysicalDevice physDevice, vk::Device device, vk::Queue queue, vk::CommandBuffer cmdBuf, void *datSrc, vk::DeviceSize sz, vk::DeviceSize offset, vk::Fence fence) {
     Buffer stagingBuf{physDevice, device, sz, vk::BufferUsageFlagBits::eTransferSrc, vk::MemoryPropertyFlagBits::eHostVisible};
     device.waitForFences({fence}, true, UINT64_MAX);
     device.resetFences({fence});
     writeByMemoryMapping(device, stagingBuf.getMemory(), datSrc, sz, 0);
-    writeByBufferCopy(device, cmdBuf, queue, stagingBuf.getBuffer(), buffer.get(), sz, 0, 0, fence);
+    writeByBufferCopy(device, cmdBuf, queue, stagingBuf.getBuffer(), buffer.get(), sz, 0, offset, fence);
     device.waitForFences({fence}, true, UINT64_MAX);
     device.resetFences({fence});
 }
